97edab5ebeafb1b46588149c145ef3efbfeeb74a
fatkhullayevsadulloxon <sadulloxonfatkhullayev@gmail.com>
2023-03-14 00:16:20 +0000

    init

--- js/index.js
+++ js/index.js
@@ -2,17 +2,23 @@
 
 const elProduct = document.querySelector(".product")
 
+const korzina = []
+
 const getCountryData = function(data) {
     for(let dataArr of data ){
         const html = `
-              <div class="col-lg-4 col-md-6 wow zoomIn" data-wow-delay="0.1s">
+              <div class="col-lg-4 card-border col-md-6 wow zoomIn" data-wow-delay="0.1s">
                         <div class="service-item d-flex flex-column justify-content-center text-center rounded">
                             <div class="service-icon flex-shrink-0">
-                                <i class="fa fa-home fa-2x"></i>
+                                <img class="img" src="${dataArr.image}" >
                             </div>
-                            <h5 class="mb-3">${dataArr.title}</h5>
-                            <p>${dataArr.id}</p>
-                            <a class="btn px-3 mt-auto mx-auto" href="">Korzinka</a>
+                            <h5 class="mb-3">${dataArr.name}</h5>
+                            <p>${dataArr.description}</p>
+                            <strong>${dataArr.price.toLocaleString("uzs", {
+                                style: 'currency',
+                                currency: 'uzs',
+                            })}</strong>
+                            <a class="btn px-3 mt-auto mx-auto" data-btn-id="${dataArr.id}" href="">Korzinka</a>
                         </div>
                     </div>
         `
@@ -22,9 +28,9 @@ const getCountryData = function(data) {
 }
 
 const getCountry = function() {
-    fetch("https://jsonplaceholder.typicode.com/todos/1/")
+    fetch("https://kansmir.iprogrammer.uz/products/all/")
     .then(res => res.json())
-    .then(data =>getCountryData(data))
+    .then(data => getCountryData(data))
 }
 
 getCountry()
