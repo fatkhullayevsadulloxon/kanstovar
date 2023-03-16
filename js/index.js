5eec6873ea05d1c7862b0e01b815b98696ff3ed9
fatkhullayevsadulloxon <sadulloxonfatkhullayev@gmail.com>
2023-03-16 12:59:23 +0000

    init

--- js/index.js
+++ js/index.js
@@ -1,36 +1,91 @@
 "use strict"
 
 const elProduct = document.querySelector(".product")
+const elProductKorzina = document.querySelector(".product-korzina")
+const elKorzina = document.querySelector(".korzina")
+const elLoading = document.querySelector(".loading2")
+// const elResult = document.querySelector(".result")
+const elHullas = document.querySelector(".hullas")
+const elInputName = document.querySelector(".input-name")
+const elInputLast = document.querySelector(".input-last")
+const elInputNumber = document.querySelector(".input-number")
+const elFormData = document.querySelector(".form-data")
 
-const korzina = []
 
-const getCountryData = function(data) {
-    for(let dataArr of data ){
+let lstore;
+
+lstore = JSON.parse(window.localStorage.getItem("korzina"))
+
+const korzina = lstore || []
+
+const getCountryData = function (data) {
+    for (let dataArr of data) {
         const html = `
               <div class="col-lg-4 card-border col-md-6 wow zoomIn" data-wow-delay="0.1s">
                         <div class="service-item d-flex flex-column justify-content-center text-center rounded">
                             <div class="service-icon flex-shrink-0">
-                                <img class="img" src="${dataArr.image}" >
+                                <img class="img mt-5" src="${dataArr.image}" >
                             </div>
-                            <h5 class="mb-3">${dataArr.name}</h5>
-                            <p>${dataArr.description}</p>
-                            <strong>${dataArr.price.toLocaleString("uzs", {
-                                style: 'currency',
-                                currency: 'uzs',
-                            })}</strong>
-                            <a class="btn px-3 mt-auto mx-auto" data-btn-id="${dataArr.id}" href="">Korzinka</a>
+                            <h5 class="mb-3 heading mt-5">${dataArr.name}</h5>
+                            <p class="heading2">${dataArr.description.substr(0, 60)}...</p>
+                            <strong class="heading buy">${dataArr.price.toLocaleString("uzs", {
+            style: 'currency',
+            currency: 'uzs',
+        })}</strong>
+                            <a class="btn px-3 mt-auto mx-auto btn-book heading2" data-btn-id="${dataArr.id}" href="#!">Korzinka</a>
                         </div>
                     </div>
         `
-
         elProduct.insertAdjacentHTML("beforeend", html)
     }
+
+    elProduct.addEventListener("click", (e) => {
+        const isBookmarkBtn = e.target.matches(".btn-book")
+
+
+        if (isBookmarkBtn) {
+            const filmId = e.target.dataset.btnId;
+            elResult.textContent = count++
+
+            fetch(`https://kansmir.iprogrammer.uz/product/get/${filmId}`)
+                .then(res => res.json())
+                .then(data => {
+                    korzina.push(data)
+                    window.localStorage.setItem("korzina", JSON.stringify(korzina))
+                })
+        }
+
+    })
+    
 }
 
-const getCountry = function() {
+elFormData.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const inputValueName = elInputName.value
+    const inputValueLast = elInputLast.value
+    const inputValueNum = elInputNumber.value
+
+    fetch('https://kansmir.iprogrammer.uz/book/create/', {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+        },
+        body: JSON.stringify({
+            first_name: inputValueName,
+            last_name: inputValueLast,
+            phone: inputValueNum,
+        })
+    })
+    .then((res) => res.json())
+    .then((data) => {
+        window.location.replace("index.html")
+    })
+})
+
+const getCountry = function () {
     fetch("https://kansmir.iprogrammer.uz/products/all/")
-    .then(res => res.json())
-    .then(data => getCountryData(data))
+        .then(res => res.json())
+        .then(data => getCountryData(data))
 }
-
 getCountry()
---
+++ js/korzinka.js
@@ -0,0 +1,49 @@
+const elList = document.querySelector(".product-korzina")
+const elClearBtn = document.querySelector(".clear-btn")
+const elProductResult = document.querySelector(".product-result")
+
+let lstore;
+
+lstore = JSON.parse(window.localStorage.getItem("korzina"))
+
+
+
+const korzina = lstore || []
+
+if(korzina.length){
+  elClearBtn.classList.remove("d-none")
+}
+
+const getCountryDataBookmark = function () {
+  for (let bookmark of korzina) {
+    const html = `
+              <div class="col-lg-4 card-border col-md-6 wow zoomIn" data-wow-delay="0.1s">
+                        <div class="service-item d-flex flex-column justify-content-center text-center rounded">
+                            <div class="service-icon flex-shrink-0">
+                                <img class="img" src="${bookmark.image}" >
+                            </div>
+                            <h5 class="mb-3">${bookmark.name}</h5>
+                            <p>${bookmark.description}</p>
+                            <strong>${bookmark.price.toLocaleString("uzs", {
+      style: 'currency',
+      currency: 'uzs',
+    })}</strong>
+    <a type="button" href="tel:+998930550595" class="btn mx-auto" data-bs-toggle="modal" data-bs-target="#exampleModal">
+      Sotib olish
+    </a>
+
+                        </div>
+                    </div>
+        `
+    elList.insertAdjacentHTML("beforeend", html)
+  }
+}
+
+elClearBtn.addEventListener("click", (e) => {
+
+  window.localStorage.removeItem("korzina")
+  window.location.reload(true)
+
+})
+
+getCountryDataBookmark()
